110d94427667972a6be9c0762fc44365ada27d72
eraymond1 <ericraymond@rocketmail.com>
2022-04-17 21:58:04 -0400

    fixed logic so only correct input is taken at prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,40 +14,95 @@ var length = 0;
 var password = "";
 var pass = "";
 
+var con = false;
 
 
-while (length < 8) {
+
+
+
+while (length < 8 || length > 128) {
+
   length = window.prompt("Please enter length of password.");
-  if (length < 8){
-    window.alert("Requires a min of 8 charecters!")
+
+  if (length < 8 || length > 128){
+    window.alert("Requires a min of 8 charecters!");   
+    
   } else {
-    pass = num;
+    pass += num;
     break;
+  }
+
+}
+
+var upper = "";
+
+while (con === false) {
+
+  upper = window.prompt("Do you want uppercase letters? Please type yes or no.");
+
+  
+  
+  if (upper.toLowerCase() === "no" || upper.toLowerCase() === "n") {
+    
+    
+    break;
+    
+  }else if (upper.toLowerCase() === "yes" || upper.toLowerCase() === "y") {
+    pass += alphaUp;
+    break;
+
+  }else  {
+    window.alert("You need to make a valid selection. Please try again!");
+    
+  }
+
+
+}
+
+var lower = "";
+
+while (con === false){
+  lower = window.prompt("Do you want lowercase letters? Please type yes or no.");
+
+  if (lower.toLowerCase() === "no" || lower.toLowerCase() === "n"){
+    
+    break;
+    
+  }else if (lower.toLowerCase() === "yes" || lower.toLowerCase() === "y") {
+    pass += alphaLc;
+    break;
+  }else {
+    window.alert("You need to make a valid selection. Please try again!");
   }
 }
 
-var upper = window.prompt("Do you want uppercase letters?")
+var sym = "";
 
-if (upper === "" || upper === null) {
-  window.alert("You need to make a selection!");
-  upper = window.prompt("Do you want uppercase letters?")
+while (con === false) {
+  sym = window.prompt("Do you want to use special characters? Please type yes or no.")
 
-}else if (upper === "no"){
+  if (sym.toLowerCase() === "no" || sym.toLowerCase() === "n"){
 
-}else if (upper === "yes"){
-  pass += alphaUp;
+    break;
+    
+  }else if (sym.toLowerCase() === "yes" || sym.toLowerCase() === "y") {
+
+    pass += symbols;
+    break;
+
+  }else {
+    window.alert("You need to make a valid selection. Please try again!");
+  }
+
 }
 
-var lower = window.prompt("Do you want lowercase letters?")
 
-if (lower === "" || lower === null){
-  window.alert("You need to make a selection!");
-  lower = window.prompt("Do you want lowercase letters?")
-}else if (upper === "no"){
 
-}else if (upper === "yes"){
-  pass += alphaLc;
-}
+
+
+
+
+
 num = pass;
 
 for (var i= 0; i < length; i++){
@@ -55,7 +110,7 @@ for (var i= 0; i < length; i++){
 
   password += pass.substring(random, random + 1);
 }
-console.log("clicked");
+
 
 
 
