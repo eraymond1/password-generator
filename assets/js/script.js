4da389ff960dbd43304289713e29236d2a87e6cd
eraymond1 <ericraymond@rocketmail.com>
2022-04-17 22:13:38 -0400

    organized and commented code for clarity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 // Assignment code here
+
+
 var generateBtn = document.querySelector("#generate");
 
-
-
-
-
+// generatePassword function is used to pass password criteria to writePassword()
 function generatePassword() {
+
+  /* The variables below are used to hold uppercase, lowercase, special characters, and numbers for the 
+  creation of the password*/
 var alphaUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var alphaLc = "abcdefghijklmnopqrstuvwxyz";
 var symbols = "!@#$%^&*_-+=";
 var num = "0123456789";
-var length = 0;
-var password = "";
-var pass = "";
 
+//length was used for the number prompt for input taken by user
+var length = 0;
+//var password is used in for loop to wrap the password together for output
+var password = "";
+//var pass is used to collect password criteria to hand off to password 
+var pass = "";
+// con is for while loop conditions until condition is validated true.
 var con = false;
 
 
 
 
-
+// while loop to check for min of 8 and max of 128
 while (length < 8 || length > 128) {
 
   length = window.prompt("Please enter length of password.");
@@ -34,20 +40,21 @@ while (length < 8 || length > 128) {
 
 }
 
+
+//declared upper above loop so it was clear this is used in the upper case loop
 var upper = "";
 
+//loop checks that user in inputing a yes or a no and I used .toLowerCase() to take all input and make it lower case to minimize conditional checks
 while (con === false) {
 
   upper = window.prompt("Do you want uppercase letters? Please type yes or no.");
-
-  
   
   if (upper.toLowerCase() === "no" || upper.toLowerCase() === "n") {
-    
     
     break;
     
   }else if (upper.toLowerCase() === "yes" || upper.toLowerCase() === "y") {
+
     pass += alphaUp;
     break;
 
@@ -55,12 +62,12 @@ while (con === false) {
     window.alert("You need to make a valid selection. Please try again!");
     
   }
-
-
 }
 
+//declared lower here to make sure it was known this is for the below loop
 var lower = "";
 
+//loop checks that user in inputing a yes or a no and I used .toLowerCase() to take all input and make it lower case to minimize conditional checks
 while (con === false){
   lower = window.prompt("Do you want lowercase letters? Please type yes or no.");
 
@@ -76,8 +83,10 @@ while (con === false){
   }
 }
 
+//declared sym here to make it known this was for below loop
 var sym = "";
 
+//loop checks that user in inputing a yes or a no and I used .toLowerCase() to take all input and make it lower case to minimize conditional checks
 while (con === false) {
   sym = window.prompt("Do you want to use special characters? Please type yes or no.")
 
@@ -96,24 +105,17 @@ while (con === false) {
 
 }
 
-
-
-
-
-
-
-
-num = pass;
-
+//for loop interates through the given length to create the random password with all the criteria selected
 for (var i= 0; i < length; i++){
+
+  //random uses Math.floor to round off decimal and math.random to multiply the password
   var random = Math.floor(Math.random() * pass.length);
 
+  //use substring here to wrap everything together to give speficied length of password by user
   password += pass.substring(random, random + 1);
 }
 
-
-
-
+//return statement allows the data from function to be called
 return password;
   
 };
